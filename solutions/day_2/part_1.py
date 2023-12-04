68b8446b84cbcc2ecde35aaa196b2fb928e3d394
Joaquín Neira <github@joaquinneira.fastmail.com>
2023-12-04 20:39:40 -0300

    day 2

---
+++ solutions/day_2/part_1.py
@@ -0,0 +1,53 @@
+from solutions.common.file_reader import read_file_lines
+
+
+def get_game_number(game_description: str) -> int:
+    return int(game_description.split(" ")[1])
+
+
+def get_game_info(game: str) -> dict[str, int]:
+    game_info: dict[str, int] = {}
+    for revealed in game.split(","):
+        number, color = revealed.strip().split(" ")
+        game_info[color] = int(number)
+
+    return game_info
+
+
+def get_revealed_numbers(games: str):
+    game_list = games.split(";")
+    return [get_game_info(game) for game in game_list]
+
+
+def process_line(line: str):
+    game_description, games = line.split(":")
+
+    return get_game_number(game_description), get_revealed_numbers(games)
+
+
+def check_possible_reveal(
+    revealed: dict[str, int], total_balls: dict[str, int]
+) -> bool:
+    for color in revealed:
+        if revealed[color] > total_balls[color]:
+            return False
+    return True
+
+
+def check_possible_game(
+    game: list[dict[str, int]], total_balls: dict[str, int]
+) -> bool:
+    return all(check_possible_reveal(revealed, total_balls) for revealed in game)
+
+
+if __name__ == "__main__":
+    existing_balls = {"red": 12, "green": 13, "blue": 14}
+    ids_sum = 0
+
+    for line in read_file_lines("day_2.txt"):
+        game_number, games = process_line(line)
+
+        if check_possible_game(games, existing_balls):
+            ids_sum += game_number
+
+    print(ids_sum)
---
+++ solutions/day_2/part_2.py
@@ -0,0 +1,51 @@
+from functools import reduce
+from solutions.common.file_reader import read_file_lines
+
+
+def get_game_number(game_description: str) -> int:
+    return int(game_description.split(" ")[1])
+
+
+def get_game_info(game: str) -> dict[str, int]:
+    game_info: dict[str, int] = {}
+    for revealed in game.split(","):
+        number, color = revealed.strip().split(" ")
+        game_info[color] = int(number)
+
+    return game_info
+
+
+def get_revealed_numbers(games: str) -> list[dict[str, int]]:
+    game_list = games.split(";")
+    return [get_game_info(game) for game in game_list]
+
+
+def process_line(line: str) -> tuple[int, list[dict[str, int]]]:
+    game_description, games = line.split(":")
+
+    return get_game_number(game_description), get_revealed_numbers(games)
+
+
+def get_minimum_number_of_cubes(game: list[dict[str, int]]) -> dict[str, int]:
+    total_cubes = {"red": 0, "green": 0, "blue": 0}
+
+    for revealed in game:
+        for color in revealed:
+            if revealed[color] > total_cubes[color]:
+                total_cubes[color] = revealed[color]
+
+    return total_cubes
+
+
+if __name__ == "__main__":
+    existing_cubes = {"red": 12, "green": 13, "blue": 14}
+    power_sum = 0
+
+    for line in read_file_lines("day_2.txt"):
+        game_number, games = process_line(line)
+
+        power = reduce(lambda x, y: x * y, get_minimum_number_of_cubes(games).values())
+
+        power_sum += power
+
+    print(power_sum)
